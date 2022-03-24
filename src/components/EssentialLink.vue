aa79abe5cf5776270e7266f8198c7a288e0cbd06
PHILEMON SHEKARI <holyphilzy@gmail.com>
2022-03-24 22:15:18 +0100

    rebuilding of the entire lading page

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,8 +1,11 @@
 <template>
+  
   <q-item
-
+    v-if="link.includes('#')"
     clickable
-    :to="{name: link}"
+    :href="link"
+    
+    
   >
     <q-item-section
       v-if="icon"
@@ -18,6 +21,30 @@
       </q-item-label>
     </q-item-section>
   </q-item>
+
+ <q-item
+    v-else
+    clickable
+    :to="link"
+    
+    
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>
+        {{ caption }}
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+
+  
 </template>
 
 <script>
