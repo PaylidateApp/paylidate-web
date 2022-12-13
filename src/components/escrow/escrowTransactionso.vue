e3aa0c1cdc9177c9e9d12b959dfca117eabf3acc
victoria <victoriaowolijah@gmail.com>
2022-12-13 21:18:04 +0100

    changes

--- src/components/escrow/escrowTransactionso.vue
+++ src/components/escrow/escrowTransactionso.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="q-pa-md">
-    <q-card flat square class=" column flex-center">
+  <div>
+    <q-card flat square class="bg-primary column flex-center">
       <q-card-section class="column flex-center q-gutter-sm">
-        <div class="text-h5 text-bold text-dark">
-          Transactions
+        <div class="text-h5 text-bold text-secondary">
+          Escrow Transactions
         </div>
         <div class="row q-gutter-sm">
           <!-- <sell />
@@ -13,48 +13,34 @@
         </div>
       </q-card-section>
     </q-card>
-    <!-- <q-card flat square class="bg-primary">
-      <q-card-section class="row flex-center no-padding q-gutter-sm">
-        <div class="text-secondary text-center text-uppercase">
-          <div class="text-h5 text-bold">{{currency(sum_ammount('received',user.id))}}</div>
-          <div style="font-size: 10px">PAYMENTS RECEIVED</div>
-        </div>
-        <q-separator spaced vertical dark />
-        <div class="text-secondary text-center text-uppercase">
-          <div class="text-h5 text-bold">{{currency(sum_ammount('made',user.id))}}</div>
-          <div style="font-size: 10px">PAYMENTS MADE</div>
-        </div>
-      </q-card-section>
-    </q-card> -->
+    
 
     <div>
 
-      <!--
-###TODO <br>
-add confirm or reject request <br>
-add disbut messaging <br>
-add status badge <br>
-add payment remitance date automatically on all transaction <br> -->
-
-      <!-- {{ contents[0] }} -->
-
+      
     </div>
 
-    <q-table title="" :data="contents" :columns="columns" row-key="name" square :dense="$q.screen.lt.md">
+    <!-- <q-table title="Transactions" :data="contents" :columns="columns" row-key="name" square>
       <template v-slot:top-right>
-        <!-- <eExport :array="contents" :columns="columns" /> -->
+        <eExport :array="contents" :columns="columns" /> -->
         <!-- <q-btn unelevated color="primary" icon-right="archive" label="Download Table" no-caps @click="exportTable" /> -->
       </template>
 
-      <template v-slot:body="props" :dense="$q.screen.lt.md">
-        <q-tr :props="props" :dense="$q.screen.lt.md">
+      <template v-slot:body="props">
+        <q-tr :props="props">
           <!-- <q-td key="name" :props="props">
             <img :src="props.row.image" style="width: 50px" />
           </q-td> -->
-          
-          <q-td key="name" :props="props" :id="'product_' + props.row.product.id" @mouseover="showFullText('product_' + props.row.product.id)">
-            <span id="showslice">{{ props.row.product.name.slice(0,5) }}</span>
-            <span id="showfull" :style.display="block">{{ props.row.product.name}}</span>
+          <q-td key="transaction_ref" :props="props">
+
+            <q-btn class="text-blue" :to="{name: 'transaction', params: {T_ref: props.row.transaction_ref}}" flat
+              size="md" no-caps>
+
+              view
+            </q-btn>
+          </q-td>
+          <q-td key="name" :props="props">
+            {{ props.row.product.name }}
           </q-td>
           <q-td key="quantity" :props="props">
             <span class="text-bold">{{ props.row.quantity }}</span>
@@ -83,42 +69,7 @@ add payment remitance date automatically on all transaction <br> -->
             </q-badge>
           </q-td>
 
-          <!--<q-td key="confirmed" :props="props">
-            <q-badge :color="props.row.secondary_user_id ? 'steal' : 'negative'">
-              {{ props.row.secondary_user_id ? 'Confirmed' : 'Un-confirmed' }}
-            </q-badge>
-          </q-td> -->
-          <!-- <q-td key="payment_status" :props="props">
-            <q-badge v-if="props.row.payment_status" color="steal">
-               {{ 'Paid' }}
-            </q-badge>
-            <q-badge v-else-if="props.row.type === 'sell' && !props.row.payment" color="negative">
-               {{ 'Un-Paid' }}
-            </q-badge>
-            <Payment v-else :amount="props.row.price" :T_ref="props.row.T_ref" :product="props.row" :url="payment_url+props.row.T_ref+'/payment'"/>
-          </q-td>
-          <q-td key="link" :props="props">
-            <q-btn label="Copy Link" @click="copy_link(props.row.transaction_ref)" flat size="sm" no-caps
-              class="bg-grey">
-              <q-tooltip>
-                {{copyL}}
-              </q-tooltip>
-            </q-btn>
-          </q-td>-->
-
-          <!-- <q-td key="action" :props="props" class=""
-            v-if="props.row.type === 'sell' && props.row.user_id == user.id || props.row.type === 'buy' && props.row.user_id != user.id" >
-            <q-btn v-if="props.row.delivery_status == 0" @click="startDelivery(props.row.id)" color="warning" size="xs" no-caps label="Start Delivery" />
-            <q-btn v-if="props.row.delivery_status == 1" @click="orderDelivered(props.row.id)" color="negative" size="xs" no-caps label="Order Completed" />
-            <Disput v-if="props.row.delivery_status == 1" />
-          </q-td>
-          <q-td key="action" :props="props" class=""
-            v-if="props.row.type === 'buy' && props.row.user_id == user.id || props.row.type === 'sell' && props.row.user_id != user.id">
-            <span v-if="props.row.delivery_status == 0" class="text-bold">Awaiting Delivery</span>
-            <q-btn v-if="props.row.delivery_status == 1" @click="orderRecieved(props.row.id)" color="warning" size="xs" no-caps label="Recieved Delivery" />
-            <Disput  />
-          </q-td> -->
-
+          
 
           <!-- TODO paid unpaid in-dispute complete -->
 
@@ -146,16 +97,40 @@ add payment remitance date automatically on all transaction <br> -->
               Cancelled
             </q-badge>
           </q-td>
-          <q-td key="transaction_ref" :props="props">
-            <q-btn class="text-blue" :to="{name: 'transaction', params: {T_ref: props.row.transaction_ref}}" flatsize="md" no-caps>
-              view
-            </q-btn>
-          </q-td>
 
         </q-tr>
       </template>
 
     </q-table>
+
+<template>
+
+      
+      
+
+  <div class="q-pa-md" align: center id="trans">
+    <q-card flat square class=" column flex-center">
+      <q-card-section class="column flex-center q-gutter-sm">
+        <div class="text-h5 text-bold text-dark">
+          Transactions
+        </div>
+        <div class="row q-gutter-sm">
+          <!-- <sell />
+          <buy /> -->
+
+
+        </div>
+      </q-card-section>
+    </q-card>
+
+    <q-table
+      :data="contents"
+      :columns="columns"
+      row-key="name"
+      class="text-center"
+    />
+  </div>
+</template>
   </div>
 </template>
 
@@ -175,25 +150,36 @@ export default {
     return {
       columns: [
         // { name: 'src', field: 'src' },
-        { name: 'name', label: 'Product/Service', field: 'name', align: 'left' , sortable: true, },
-        { name: 'quantity', label: 'Qty', field: 'transaction', sortable: true,  align: 'left'  },
-        { name: 'amount', label: 'Price', field: 'transaction', sortable: true,  align: 'left'  },
+        { name: 'product', label: 'Product/Service', field: 'product', align: 'center' , sortable: true, },
+        { name: 'quantity', label: 'Qty', field: 'quantity', sortable: true,  align: 'center'  },
+        { name: 'amount', label: 'Price', field: 'amount', sortable: true,  align: 'center'  },
         { name: 'transaction_type', label: 'Type', field: 'transaction_type', sortable: true,  align: 'center'  },
-        // { name: 'confirmed', label: 'Confirmation', field: 'confirmed', align: 'left', sortable: true },
-        // { name: 'payment_status', label: 'Payment', field: 'payment_status',align: 'left', sortable: true },
+        // { name: 'confirmed', label: 'Confirmation', field: 'confirmed', align: 'center', sortable: true },
+        // { name: 'payment_status', label: 'Payment', field: 'payment_status',align: 'center', sortable: true },
         //{ name: 'link', label: 'Transaction link', field: '', align: 'center', sortable: true },
         { name: 'created_at', label: 'Date Created', field: 'created_at', align: 'center', sortable: true, },
-        { name: 'transaction_status', label: 'Status', field: 'transaction',  align: 'left', sortable: true },
-        { name: 'transaction_ref', label: 'Open', field: 'transaction', align: 'left' , sortable: true,},
-
+        { name: 'status', label: 'Status', field: 'status',  align: 'center', sortable: true },
+        { name: 'transaction_ref', label: 'Open', field: 'transaction_ref', align: 'center' , sortable: true,},
       ],
-      contents:[],
+
+      contents: [
+       /* {
+          transaction_ref: 'Frozen Yogurt',
+          name: "sentencessgjhjkhgkjk".slice(0,5),
+          quantity: 6.0,
+          amount: 24,
+          transaction_type: 4.0,
+          created_at: 87,
+          transaction_status: '14%'
+        },*/
+        
+      ],
+
+      
       payment_url: `${window.location.href}/product/`,
       copyLink:'Copy link',
-      
     }
   },
-  
   mounted() {
     if (!this.$q.localStorage.getItem('paylidate_token')) {
       this.$router.push({ name: "login" });
@@ -206,14 +192,6 @@ export default {
 
   },
   methods: {
-    showFullText: function(id){
-            console.log(id)
-            let parent=document.getElementById(id)
-            let showslice=parent.querySelector("#showslice")
-            let showfull=parent.querySelector("#showsfull")
-            showslice.style.display="none"
-            showfull.style.display="block"
-        },
     async getTransactions(){
         //console.log('contents');
          this.$q.loading.show({
@@ -226,8 +204,8 @@ export default {
         const res = req.data
 
         this.contents = res.data;
+        console.log(res.data)
         //console.log(this.contents)
-        
 
          this.$q.loading.hide();
         }catch(err){
@@ -287,6 +265,5 @@ export default {
         this.getTransactions();
       }
   }
-  
 }
 </script>
