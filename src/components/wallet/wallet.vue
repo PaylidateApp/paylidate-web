3e92fd163ecb2bd2c8e2b726e6808ab090bb67bd
arabomeivan <arabomeivan@protonmail.com>
2023-01-06 14:01:04 +0100

    reverted to old design for wallet page

--- src/components/wallet/wallet.vue
+++ src/components/wallet/wallet.vue
@@ -1,5 +1,171 @@
 <template>
-  <div class="flex-block flex-center">
+
+<!-- Old layout for wallet page -->
+
+<div class="flex-block flex-center ">
+    <q-dialog v-model="fundWallet">
+      <q-card style="width: 350px">
+        <q-card-section>
+          <div class="text-primary q-pt-md">
+            <strong>
+              To fund your wallet, you can either use bank transfer or pay via
+              card using flutterwave checkout <br /><br />
+            </strong>
+            <strong> Via Bank Transfer<br /> </strong>
+            {{ wallet.account_number }} <br />
+            {{ wallet.bank_name }}<br /><br />
+            <div class="flex">
+              <strong> Via flutterwave checkout (Card)<br /> </strong>
+              <q-input
+                outlined
+                dense
+                v-model="form.amount"
+                type="number"
+                label="Enter Amount"
+                maxlength="11"
+                inputmode="numeric"
+              />
+
+              <q-btn
+                class="q-ml-sm"
+                :loading="loading"
+                no-caps
+                color="primary"
+                label="Confirm"
+                @click="creditWalletWithFLW()"
+              />
+            </div>
+          </div>
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-secondary q-pr-md">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="width: 350px">
+        <q-card-section>
+          <div class="text-red">
+            You don't have a paylidate wallet, please enter your BVN below to
+            create your wallet
+          </div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input
+            dense
+            :rules="schema.bvn"
+            type="number"
+            v-model="bvn"
+            autofocus
+            @keyup.enter="createWallet()"
+          />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-secondary q-pr-md">
+          <q-btn
+            label="Create Wallet"
+            :loading="loading"
+            color="secondary"
+            @click="createWallet()"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <div
+      v-if="!prompt"
+      class=" bg-primary q-pa-md"
+      style="border-radius: 14px; max-width: 400px"
+    >
+      <div>
+        <div class="flex justify-between">
+          <div class="text-subtitle1 text-secondary">Paylidate Wallet</div>
+
+          <q-btn
+            unelevated
+            no-caps
+            color="secondary"
+            label="Fund wallet"
+            @click="fundWallet = true"
+          />
+        </div>
+        <div class="flex justify-between q-my-md">
+          <div>
+            <div class="row flex-center no-padding q-gutter-sm">
+              <div class="text-secondary text-center text-uppercase">
+                <div class="text-subtitle1 text-bold">
+                  ₦ {{ currency(parseFloat(wallet.balance)) }}
+                </div>
+                <div style="font-size: 11px">BALANCE</div>
+              </div>
+              <q-separator spaced vertical dark />
+              <div class="text-secondary text-center text-uppercase">
+                <div class="text-subtitle1 text-bold">
+                  ₦ {{ currency(parseFloat(wallet.bonus)) }}
+                </div>
+                <div style="font-size: 11px">BONUS</div>
+              </div>
+            </div>
+          </div>
+          <q-icon size="xl" color="secondary" name="account_balance_wallet" />
+        </div>
+        <div class="flex justify-between">
+          <div class="text-subtitle1 text-secondary">{{ user.name }}</div>
+          <div class="text-secondary no-padding text-center text-uppercase">
+            <div class="text-subtitle1 text-secondary">
+              {{ wallet.account_number }}
+            </div>
+            <div style="font-size: 11px">{{ wallet.bank_name }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br />
+    <div v-if="!prompt">
+      <q-table
+        title="WALLET HISTORY"
+        :data="contents"
+        :columns="columns"
+        row-key="name"
+        square
+      >
+        <template v-slot:top-right>
+          <eExport :array="contents" :columns="columns" />
+          <!-- <q-btn unelevated color="primary" icon-right="archive" label="Download Table" no-caps @click="exportTable" /> -->
+        </template>
+
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td key="type" :props="props">
+              {{ props.row.type }}
+            </q-td>
+            <q-td key="narration" :props="props">
+              {{ props.row.narration }}
+            </q-td>
+            <q-td key="amount" :props="props">
+              ₦ {{ currency(props.row.amount) }}
+            </q-td>
+            <q-td key="balance_before" :props="props">
+              ₦ {{ currency(props.row.balance_before) }}
+            </q-td>
+            <q-td key="balance_after" :props="props">
+              ₦ {{ currency(props.row.balance_after) }}
+            </q-td>
+
+            <q-td key="created_at" :props="props">
+              {{ formatDate(props.row.created_at) }}
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+    </div>
+  </div>
+
+  <!-- New Layout for wallet page -->
+
+  <!-- <div class="flex-block flex-center">
     <q-dialog v-model="fundWallet">
       <q-card style="width: 350px">
         <q-card-section>
@@ -99,9 +265,9 @@
         class="wallethistory"
       >
         <template v-slot:top-right>
-          <eExport :array="contents" :columns="columns" />
+          <eExport :array="contents" :columns="columns" /> -->
           <!-- <q-btn unelevated color="primary" icon-right="archive" label="Download Table" no-caps @click="exportTable" /> -->
-        </template>
+        <!-- </template>
 
         <template v-slot:body="props">
           <q-tr :props="props">
@@ -126,13 +292,13 @@
             </q-td>
           </q-tr>
         </template>
-      </q-table>
+      </q-table> -->
 
-      <q-table title="WALLET HISTORY" title-align="center" :data="contents" :columns="columnsformobile" row-key="name" square class="wallethistoryformobile">
+      <!-- <q-table title="WALLET HISTORY" title-align="center" :data="contents" :columns="columnsformobile" row-key="name" square class="wallethistoryformobile">
             <template v-slot:top-right>
-              <eExport :array="contents" :columns="columnsformobile" />
+              <eExport :array="contents" :columns="columnsformobile" /> -->
               <!-- <q-btn unelevated color="primary" icon-right="archive" label="Download Table" no-caps @click="exportTable" /> -->
-            </template>
+            <!-- </template>
 
             <template v-slot:body="props">
               <q-tr :props="props">
@@ -151,10 +317,10 @@
             </template>
 
           </q-table>
-        </div>
+        </div> -->
 
     <!-- </div> -->
-  </div>
+  <!-- </div> -->
 </template>
 
 <script>
@@ -390,7 +556,7 @@ export default {
 </script>
 
 <style scoped>
-.wallet {
+/* .wallet {
   margin-left: auto;
   margin-right: auto;
   border-radius: 30px;
@@ -503,25 +669,25 @@ export default {
   .bonuscontainer {
     width: auto;
 
-  }
+  } */
 
   /**d */
-  .walletsection, .walletbalance, .useract
+  /* .walletsection, .walletbalance, .useract
   {
     justify-content:space-between;
   }
   .wallethistory {
     display: none;
   }
-}
+} */
 
 
 /**for tablets upwards to hide table with 3 columns */
-@media only screen and (min-width: 768px) {
+/* @media only screen and (min-width: 768px) {
   .wallethistoryformobile {
     display: none;
   }
-}
+} */
 
 /**for medium screens and upwards table will be disabled */
 </style>
