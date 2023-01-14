3b99ec038b6782c68170a95e4efdedebbda85b10
victoria <victoriaowolijah@gmail.com>
2023-01-14 21:48:15 +0100

    makepayment

--- src/components/escrow/escrowTransactions.vue
+++ src/components/escrow/escrowTransactions.vue
@@ -9,6 +9,36 @@
         </div>
       </q-card-section>
     </q-card>
+
+    <!-- <q-card flat square class="bg-primary">
+      <q-card-section class="row flex-center no-padding q-gutter-sm">
+        <div class="text-secondary text-center text-uppercase">
+          <div class="text-h5 text-bold">{{currency(sum_ammount('received',user.id))}}</div>
+          <div style="font-size: 10px">PAYMENTS RECEIVED</div>
+        </div>
+        <q-separator spaced vertical dark />
+        <div class="text-secondary text-center text-uppercase">
+          <div class="text-h5 text-bold">{{currency(sum_ammount('made',user.id))}}</div>
+          <div style="font-size: 10px">PAYMENTS MADE</div>
+        </div>
+      </q-card-section>
+    </q-card> -->
+
+
+    <div>
+
+      <!--
+###TODO <br>
+add confirm or reject request <br>
+add disbut messaging <br>
+add status badge <br>
+add payment remitance date automatically on all transaction <br> -->
+
+      <!-- {{ contents[0] }} -->
+
+    </div>
+
+    
     
 
     
@@ -22,6 +52,8 @@
       :dense="$q.screen.lt.md"
     >
       <template v-slot:top-right>
+        <!-- <q-table title="Transactions" :data="contents" :columns="columns" row-key="name" square>
+      <template v-slot:top-right>-->
         <!-- <eExport :array="contents" :columns="columns" /> -->
         <!-- <q-btn unelevated color="primary" icon-right="archive" label="Download Table" no-caps @click="exportTable" /> -->
       </template>
@@ -80,6 +112,43 @@
               {{ props.row.product.user_id === user.id ? "sell" : "buy" }}
             </q-badge>
           </q-td>
+
+
+          <!--<q-td key="confirmed" :props="props">
+            <q-badge :color="props.row.secondary_user_id ? 'steal' : 'negative'">
+              {{ props.row.secondary_user_id ? 'Confirmed' : 'Un-confirmed' }}
+            </q-badge>
+          </q-td> -->
+          <!-- <q-td key="payment_status" :props="props">
+            <q-badge v-if="props.row.payment_status" color="steal">
+               {{ 'Paid' }}
+            </q-badge>
+            <q-badge v-else-if="props.row.type === 'sell' && !props.row.payment" color="negative">
+               {{ 'Un-Paid' }}
+            </q-badge>
+            <Payment v-else :amount="props.row.price" :T_ref="props.row.T_ref" :product="props.row" :url="payment_url+props.row.T_ref+'/payment'"/>
+          </q-td>
+          <q-td key="link" :props="props">
+            <q-btn label="Copy Link" @click="copy_link(props.row.transaction_ref)" flat size="sm" no-caps
+              class="bg-grey">
+              <q-tooltip>
+                {{copyL}}
+              </q-tooltip>
+            </q-btn>
+          </q-td>-->
+
+          <!-- <q-td key="action" :props="props" class=""
+            v-if="props.row.type === 'sell' && props.row.user_id == user.id || props.row.type === 'buy' && props.row.user_id != user.id" >
+            <q-btn v-if="props.row.delivery_status == 0" @click="startDelivery(props.row.id)" color="warning" size="xs" no-caps label="Start Delivery" />
+            <q-btn v-if="props.row.delivery_status == 1" @click="orderDelivered(props.row.id)" color="negative" size="xs" no-caps label="Order Completed" />
+            <Disput v-if="props.row.delivery_status == 1" />
+          </q-td>
+          <q-td key="action" :props="props" class=""
+            v-if="props.row.type === 'buy' && props.row.user_id == user.id || props.row.type === 'sell' && props.row.user_id != user.id">
+            <span v-if="props.row.delivery_status == 0" class="text-bold">Awaiting Delivery</span>
+            <q-btn v-if="props.row.delivery_status == 1" @click="orderRecieved(props.row.id)" color="warning" size="xs" no-caps label="Recieved Delivery" />
+            <Disput  />
+          </q-td> -->
 
           
           <!-- TODO paid unpaid in-dispute complete -->
