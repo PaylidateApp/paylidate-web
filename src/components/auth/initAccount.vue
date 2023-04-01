1f1952f743f51cc2c5792e2ee7203ddec312e85d
Paylidate App <125457964+PaylidateApp@users.noreply.github.com>
2023-04-01 22:55:40 +0000

    edited the get banks and flutter public key

--- src/components/auth/initAccount.vue
+++ src/components/auth/initAccount.vue
@@ -98,7 +98,7 @@ export default {
     },
     makePayment(dData) {
       FlutterwaveCheckout({
-        public_key: "FLWPUBK-1fc8e0db8d628b9b475af6b74b6937e8-X",
+        public_key: "FLWPUBK-4b7b5a9ed0f871bff8f2758190718d73-X",
         tx_ref: dData.ref,
         amount: dData.amount,
         currency: "NGN",
